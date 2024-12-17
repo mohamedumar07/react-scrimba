40d04d5ba60853f534752b4b4f2c120374347ef0
mohamedumar07 <bilalumar56789@gmail.com>
2024-12-17 22:33:19 +0530

    done main content

---
+++ static web page/chef-claude/src/components/Main.jsx
@@ -0,0 +1,15 @@
+export default function Main()
+{
+    return (
+        <main>
+            <form>
+                <input 
+                aria-label="Add Ingredient" 
+                placeholder="e.g. oregano"
+                type="text" 
+                />
+                <button>+Add Ingredient</button>
+            </form>
+        </main>
+    )
+}
